fb63eb811620e07d76af633afba0df0d009753fe
Aa Sihabudin <149864830+AaSihabudin22@users.noreply.github.com>
2026-01-20 21:34:09 +0700

    Add files via upload

---
+++ src/WindowsApp1/WindowsApp1/ItemKeranjang.vb
@@ -0,0 +1,7 @@
+﻿Public Class ItemKeranjang
+    Public IdProduk As Integer
+    Public Nama As String
+    Public Harga As Decimal
+    Public Qty As Integer
+    Public Subtotal As Decimal
+End Class
---
+++ src/WindowsApp1/WindowsApp1/ModuleKoneksi.vb
@@ -0,0 +1,25 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module ModuleKoneksi
+
+    Public conn As MySqlConnection
+
+    Public Sub KoneksiDB()
+        Try
+            conn = New MySqlConnection(
+                "server=localhost;" &
+                "user id=root;" &
+                "password=;" &
+                "database=kasir_genz"
+            )
+
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Koneksi gagal: " & ex.Message)
+        End Try
+    End Sub
+
+End Module
